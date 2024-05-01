d9a4f83552573ff4f6bbd2cbb723365641d14a33
abdsaber000 <abdsaber000@git.com>
2024-05-01 18:45:59 +0200

    Add Cell class

---
+++ cell.py
@@ -0,0 +1,14 @@
+class Cell:
+
+    def __init__(self,x , y):
+        self.x = x
+        self.y = y
+    
+    #TODO
+    def open():
+        return
+    
+    #TODO    
+    def next_mark():
+        return
+    
