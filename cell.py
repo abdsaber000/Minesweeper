2f53cf7a8f0598fa778d7aa7844bec0f930980e8
abdsaber000 <abdsaber000@git.com>
2024-05-01 19:02:43 +0200

    Add Documentation to Cell

--- cell.py
+++ cell.py
@@ -1,14 +1,32 @@
+from enum import Enum
+
+
+class CellState(Enum):
+    OPENED = 1
+    MARKED = 2
+    CLOSED = 3
+
 class Cell:
 
-    def __init__(self,x , y):
+    def __init__(self, x : int , y : int) -> None:
         self.x = x
         self.y = y
+        self.state = CellState.CLOSED
     
     #TODO
-    def open():
+    """
+        open method check if we can open the cell or not.
+        if we can change the state to OPENED
+        
+        a cell can be opened if it's not marked or 
+    """
+    def open(self) -> None:
         return
     
     #TODO    
-    def next_mark():
+    """
+        if cell is opened ignore, change state from OPENED to MARKED and vice versa.
+    """
+    def flip_mark(self):
         return
     
