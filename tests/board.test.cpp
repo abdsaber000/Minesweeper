2eca1cbc3d74ca7bc8b1c2a00fcbea28b1e771b8
abdsaber000 <abdsaber000@git.com>
2024-05-09 23:18:44 +0200

    Add get_cell tests

--- tests/board.test.cpp
+++ tests/board.test.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<cassert>
 #include "../header/board.h"
 #include "../exceptions/game_exception.h"
 #include "GameTest.h"
@@ -14,44 +13,84 @@ void test_create_board(){
     auto board = Board::create_board(EASY); 
     game_test->add_test(new TestCase(
         board->get_col_size() == EASY_BOARD_SIZE,
-        "Check column size equal to easy board column size"
+        "should equal easy board column size"
     ));
 
     game_test->add_test(new TestCase(
         board->get_col_size() == EASY_BOARD_SIZE,
-        "Check row size equal to easy board row size"
+        "should equal easy board row size"
     ));
 
     board = Board::create_board(MEDIUM);
 
     game_test->add_test(new TestCase(
         board->get_col_size() == MEDIUM_BOARD_SIZE,
-        "Check column size equal to medium board column size"
+        "should equal medium board column size"
     ));
 
     game_test->add_test(new TestCase(
         board->get_col_size() == MEDIUM_BOARD_SIZE,
-        "Check row size equal to medium board row size"
+        "should equal medium board row size"
     ));
 
     board = Board::create_board(HARD);
 
     game_test->add_test(new TestCase(
         board->get_col_size() == HARD_BOARD_SIZE,
-        "Check column size equal to hard board column size"
+        "should equal hard board column size"
     ));
 
     game_test->add_test(new TestCase(
         board->get_col_size() == HARD_BOARD_SIZE,
-        "Check row size equal to hard board row size"
+        "should equal hard board row size"
     ));
    
+}
+
+void try_cell(int row , int col, bool status , Board* board){
+    try{
+        board->get_cell(row, col);
+        game_test->add_test(new TestCase(
+            status,
+            "should access valid cell"
+        ));
+    }catch(GameException *error){
+        game_test->add_test(new TestCase(
+            !status,
+            error->error_message()
+        ));
+    }
+}
+
+void try_valid_cell(int row, int col , Board* board){
+    try_cell(row , col , true, board);
+}
+
+void try_wrong_cell(int row, int col , Board* board){
+    try_cell(row , col , false, board);
+}
+
+void test_get_cell(){
+    auto board = Board::create_board(EASY);
+
+    try_valid_cell(0,0,board);
+    try_valid_cell(5,5,board);
+    try_valid_cell(EASY_BOARD_SIZE - 1,0,board);
+    try_valid_cell(0,EASY_BOARD_SIZE - 1,board);
+    try_valid_cell(EASY_BOARD_SIZE - 1,EASY_BOARD_SIZE - 1,board);
+
+
+    try_wrong_cell(-1,0,board);
+    try_wrong_cell(0,-1,board);
+    try_wrong_cell(-1,-1,board);
+    try_wrong_cell(EASY_BOARD_SIZE,0,board);
+    try_wrong_cell(0,EASY_BOARD_SIZE,board);
 }
 
 int main(){
     game_test = new GameTest("board test");
     test_create_board();
-
+    test_get_cell();
 
     game_test->run();
 }
