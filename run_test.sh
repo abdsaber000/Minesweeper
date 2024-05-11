6fb132b72be185a49ec1205d0845e5559a4262ae
abdsaber000 <abdsaber000@git.com>
2024-05-11 10:35:47 +0200

    edit scripts

--- run_test.sh
+++ run_test.sh
@@ -2,18 +2,18 @@
 
 #board tests
 g++ ./source/*.cpp ./tests/board.test.cpp -o board_test
-
 ./board_test
-
 rm board_test
 
 #cell tests
 g++ ./source/*.cpp ./tests/cell.test.cpp -o cell_test
-
 ./cell_test
-
-
 rm cell_test
+
+#game tests
+g++ ./source/*.cpp ./tests/game.test.cpp -o game_test
+./game_test
+rm game_test
 
 #boardUi tests
 g++ ./source/*.cpp ./tests/boardUiTest.cpp -o board_ui_test
